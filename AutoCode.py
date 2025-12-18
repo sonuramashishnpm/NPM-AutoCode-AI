6d13747bcfc5b6f56fea456cc39b9ce4aeea2b78
Sonu Kumar(Viral Boy Bihar)-sonukumarviralboy <sonuramashishnpm@gmail.com>
2025-12-18 18:19:59 +0530

    Change user query input method in AutoCode.py

--- AutoCode.py
+++ AutoCode.py
@@ -7,7 +7,7 @@ prompt = PromptTemplate(
     template="Hey you are helpfull code assistant that write code just write code nothing else of it and maintain proper indentation and no hectics just imagine you will give the code and we will execute it, you will be asked to generate code aboout a query generate code,this is the query:{input}"
 )
 
-user_actual_query = "Write a python code that create file and where you have to give notes also of NCERT Class 9th Social Science Chapter Democratic Rights right animated and fully interesting notes of this chapter you have to generate notes also and save that note with this file name Democratic Rights Notes this file is not created so create also and generate note also and save also "
+user_actual_query = input("Describe the task you want to automate with NPM AutoCode AI:")
 
 llm = Grok()
 
